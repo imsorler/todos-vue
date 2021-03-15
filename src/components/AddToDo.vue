b446b17a8ded4de3c8257c4f697665cc1ea9c9db
imsorler <imsorler@gmail.com>
2021-03-15 23:53:51 +0300

    initial

---
+++ src/components/AddToDo.vue
@@ -0,0 +1,42 @@
+<template>
+    <form @submit.prevent="onSubmit">
+        <input type="text" v-model="newTitle">
+        <button type="submit">Create</button>
+    </form>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            newTitle: ''
+        }
+    },
+    methods: {
+        onSubmit() {
+            if(this.newTitle.trim()) {
+                const newTodo = {
+                    id: Date.now(),
+                    title: this.newTitle,
+                    completed: false
+                }
+
+                this.$emit('add-todo', newTodo)
+                this.newTitle = ''
+            }
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+
+    input {
+        width: 400px;
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+
+        <p>it's todos app</p>
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
